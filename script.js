44e3bf3331ccc02a18668ec7ebfc806f2e43fa63
Geisbelly <geisbelly19@gmail.com>
2024-09-23 01:45:02 -0300

    Versão 1.2.0

--- script.js
+++ script.js
@@ -13,38 +13,59 @@ var concluidas = obterListaDoLocalStorage("tarefasConcluidas");
 function adicionarItem() {
     var lista = document.getElementById('tarefas-pendentes');
     var input = document.getElementById('nova-tarefa');
+    var data = document.getElementById('nova-prazo');
+    var desc = document.getElementById('nova-descricao');
+
+    var novaTarefa = {
+        nome: input.value,
+        data: data.value,
+        descricao: desc.value
+    }
     
-    var novoItem = criarItemTarefa(input.value);
+    var novoItem = criarItemTarefa(novaTarefa);
     lista.appendChild(novoItem);
 
-    pendentes.push(input.value);
+    pendentes.push(novaTarefa);
     salvarListaNoLocalStorage("tarefasPendentes", pendentes);
 
     input.value = '';
+    data.value = '';
+    desc.value = '';
 }
 
 function moverItemParaConcluidas(checkbox) {
     var lista2 = document.getElementById('tarefas-concluidas');
-    var item = checkbox.parentNode;
+    var item = checkbox.closest('li');
     lista2.appendChild(item);
     item.classList.add('completas');
 
-    var texto = item.textContent.replace('Remover', '').trim();
-    pendentes = pendentes.filter(t => t !== texto);
-    concluidas.push(texto);
+
+    var texto = checkbox.closest('li').querySelector('.nomeTarefa').textContent.trim();
+    var tarefaConcluida = pendentes.find(t => t.nome === texto);
+
+    if(tarefaConcluida){
+        pendentes = pendentes.filter(t => t.nome !== texto);
+        concluidas.push(tarefaConcluida);
+    }
+
     salvarListaNoLocalStorage("tarefasPendentes", pendentes);
     salvarListaNoLocalStorage("tarefasConcluidas", concluidas);
 }
 
 function moverItemParaPendentes(checkbox) {
     var lista = document.getElementById('tarefas-pendentes');
-    var item = checkbox.parentNode;
+    var item = checkbox.closest('li');
     lista.appendChild(item);
     item.classList.remove('completas');
 
-    var texto = item.textContent.replace('Remover', '').trim();
-    concluidas = concluidas.filter(t => t !== texto);
-    pendentes.push(texto);
+    var texto = checkbox.closest('li').querySelector('.nomeTarefa').textContent.trim();
+    var tarefaPendente = concluidas.find(t => t.nome === texto);
+
+    if(tarefaPendente){
+        concluidas = concluidas.filter(t => t.nome !== texto);
+        pendentes.push(tarefaPendente);
+    }
+
     salvarListaNoLocalStorage("tarefasPendentes", pendentes);
     salvarListaNoLocalStorage("tarefasConcluidas", concluidas);
 }
@@ -67,32 +88,75 @@ function carregarTask() {
     });
 }
 
+function formatarDataHora(horario){
+    var partes = horario.split('T');
+
+    var listaData = partes[0].split('-');
+    var dia = parseInt(listaData[2],10);
+    var mes = parseInt(listaData[1],10);
+    var ano = parseInt(listaData[0],10);
+
+    var horario = partes[1];
+    
+    return dia+'/'+mes+'/'+ano+'  '+horario
+}
+
+
 function editarItem(novoItem, tarefa, concluida) {
     var inputEditar = document.createElement('input');
     inputEditar.classList.add('inputEdicao')
     inputEditar.type = 'text';
-    inputEditar.value = tarefa;
-    novoItem.innerHTML = '';
-    novoItem.appendChild(inputEditar); 
+    inputEditar.value = tarefa.nome;
+
+    var inputData = document.createElement('input');
+    inputData.classList.add('inputData')
+    inputData.type = 'datetime-local';
+    inputData.value = tarefa.data;
+
+    var divPri = document.createElement('div')
+    divPri.classList.add('divPri')
+    divPri.appendChild(inputEditar)
+    divPri.appendChild(inputData)
+
+    var inputDesc = document.createElement('input');
+    inputDesc.classList.add('inputDesc')
+    inputDesc.type = 'text';
+    inputDesc.value = tarefa.descricao;
 
     var botaoSalvar = document.createElement('button');
     botaoSalvar.textContent = 'Salvar';
     botaoSalvar.style.marginLeft = '10px';
     botaoSalvar.classList.add('botaoSalvar');
 
+    var divSec = document.createElement('div')
+    divSec.classList.add('divSec')
+    divSec.appendChild(inputDesc)
+    divSec.appendChild(botaoSalvar)
+
+    var divPrin = document.createElement('div')
+    divPrin.classList.add('divPrin')
+    divPrin.appendChild(divPri)
+    divPrin.appendChild(divSec)
+
+    novoItem.innerHTML = '';
+    novoItem.appendChild(divPrin);
+
     botaoSalvar.addEventListener('click', function () {
-        var textoEditado = inputEditar.value;
+        tarefa.nome = inputEditar.value;
+        tarefa.data = inputData.value;
+        tarefa.descricao = inputDesc.value;
+
         if (concluida) {
-            concluidas = concluidas.map(t => t === tarefa ? textoEditado : t);
+            concluidas = concluidas.map(t => t.nome === tarefa.nome ? tarefa : t);
             salvarListaNoLocalStorage("tarefasConcluidas", concluidas);
         } else {
-            pendentes = pendentes.map(t => t === tarefa ? textoEditado : t);
+            pendentes = pendentes.map(t => t.nome === tarefa.nome ? tarefa : t);
             salvarListaNoLocalStorage("tarefasPendentes", pendentes);
         }
         carregarTask(); 
     });
 
-    novoItem.appendChild(botaoSalvar); 
+    
 }
 
 
@@ -101,6 +165,23 @@ function criarItemTarefa(tarefa, concluida = false) {
     var checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = concluida;
+
+    var tituloTask = document.createElement('p');
+    tituloTask.classList.add('nomeTarefa');
+    tituloTask.textContent = tarefa.nome;
+
+    var prazoTask = document.createElement('p');
+    prazoTask.classList.add('prazoTarefa');
+    prazoTask.textContent = 'Prazo: '+formatarDataHora(tarefa.data);
+
+    var descricaoTask = document.createElement('p');
+    descricaoTask.classList.add('descTarefa');
+    descricaoTask.textContent = "Descrição: "+tarefa.descricao;
+
+    var divDadosTask = document.createElement('div')
+    divDadosTask.classList.add('Dados-Task')
+    divDadosTask.appendChild(prazoTask)
+    divDadosTask.appendChild(descricaoTask)
 
     var botaoEditar = document.createElement('button');
     botaoEditar.innerHTML = '<i class="fas fa-pencil-alt"></i>';
@@ -132,10 +213,18 @@ function criarItemTarefa(tarefa, concluida = false) {
     elementoDiv.appendChild(botaoEditar)
     elementoDiv.appendChild(botaoRemover)
 
-    novoItem.appendChild(checkbox);
-    var texto = document.createTextNode(tarefa);
-    novoItem.appendChild(texto);
-    novoItem.appendChild(elementoDiv);
+    var divCabTask = document.createElement('div')
+    divCabTask.classList.add('Cab-Task')
+    divCabTask.appendChild(checkbox)
+    divCabTask.appendChild(tituloTask)
+    divCabTask.appendChild(elementoDiv)
+
+    var divTask = document.createElement('div')
+    divTask.classList.add('Div-Task')
+    divTask.appendChild(divCabTask)
+    divTask.appendChild(divDadosTask)
+
+    novoItem.appendChild(divTask);
 
     if (concluida) {
         novoItem.classList.add('completas');
